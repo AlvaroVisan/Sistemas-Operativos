d0ef8c300836af35f9da9b7d5ad610a762464891
al-arroyo <113766452+al-arroyo@users.noreply.github.com>
2023-11-20 12:36:07 +0100

    primer try server

--- practica3/ServidorVideos.java
+++ practica3/ServidorVideos.java
@@ -1,12 +1,22 @@
 package practica3;
 
+import java.io.IOException;
+
+import ssoo.videos.servidor.ReceptorPeticiones;
 
 public class ServidorVideos {
     public static void main(String[] args) {
-        for(int i = 0; i < 10; i++) {
-            HiloRecepcion hiloRecepcion = new HiloRecepcion();
-            Thread hilo = new Thread(hiloRecepcion);
-            hilo.start();
+        try {
+            ReceptorPeticiones receptorPeticiones = new ReceptorPeticiones();
+            
+            for(int i = 0; i < 10; i++) {
+                HiloRecepcion hiloRecepcion = new HiloRecepcion(receptorPeticiones.recibirPeticion());
+                Thread hilo = new Thread(hiloRecepcion);
+                hilo.start();
+            }
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
     }
 }
