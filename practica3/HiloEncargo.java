dbc666f7f3488c17d9f249dcd79ad0d8f8c56283
al-arroyo <113766452+al-arroyo@users.noreply.github.com>
2023-11-20 12:13:31 +0100

    p3 base

---
+++ practica3/HiloEncargo.java
@@ -0,0 +1,11 @@
+package practica3;
+
+
+public class HiloEncargo implements Runnable {
+
+    @Override
+    public void run() {
+        
+    }
+    
+}
---
+++ practica3/HiloRecepcion.java
@@ -0,0 +1,10 @@
+package practica3;
+
+public class HiloRecepcion implements Runnable {
+
+    @Override
+    public void run() {
+        
+    }
+    
+}
